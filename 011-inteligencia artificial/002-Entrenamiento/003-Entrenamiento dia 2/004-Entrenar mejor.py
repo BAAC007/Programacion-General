5bb1727f1ccf0aa6eb06c5d0ea1a0b748dc293f7
Bryan Avila <bc714006@gmail.com>
2026-02-18 11:00:20 +0100

    update

--- 011-inteligencia artificial/002-Entrenamiento/003-Entrenamiento dia 2/004-Entrenar mejor.py
+++ 011-inteligencia artificial/002-Entrenamiento/003-Entrenamiento dia 2/004-Entrenar mejor.py
@@ -22,7 +22,7 @@ from peft import LoraConfig, get_peft_model
 # ------------------------------------------------------------
 # CONFIG
 # ------------------------------------------------------------
-DATA_FILE = "004-preentrenamiento relleno.jsonl"
+DATA_FILE = "/var/www/html/mi_area/Materias/Programacion-General/011-inteligencia artificial/002-Entrenamiento/003-Entrenamiento dia 2/004-Preentrenamiento relleno.jsonl"
 
 # Primary (too big for 8GB RAM unless quantized)
 PRIMARY_MODEL = "microsoft/Phi-3-mini-4k-instruct"
